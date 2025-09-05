b7bbbd1ecde4db61d3bacda507c4bfe646d44ea7
Tim McEwan <timmcewandev@gmail.com>
2025-09-04 20:26:20 -0600

    Initial Commit

---
+++ CricutTech/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CricutTech
+//
+//  Created by Tim McEwan on 9/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CricutTech/CricutTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricutTechApp.swift
+//  CricutTech
+//
+//  Created by Tim McEwan on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CricutTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
